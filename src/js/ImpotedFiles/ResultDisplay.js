e257d0cf13e27c5e6775ca20731656cb993152cc
Arshnoor Kirmanin <arshnoorkirmani786@gmail.com>
2024-03-29 22:13:38 +0530

    ResultDisplay logic complete

--- src/js/ImpotedFiles/ResultDisplay.js
+++ src/js/ImpotedFiles/ResultDisplay.js
@@ -4,8 +4,17 @@ Parcent = document.querySelector(".Percente");
 RightAnswerElement = document.querySelector(".Right >span");
 WorngAnswer = document.querySelector(".Worng >span");
 TotalQuestions = document.querySelector(".Atempt >span");
+
+// Fill Datas
+TotalQuestions.innerText = localStorage.getItem("TotalQuestions");
+RightAnswerElement.innerText = localStorage.getItem("RightAnswer");
+WorngAnswer.innerText = TotalQuestions.innerText - RightAnswerElement.innerText;
+Parcent.innerText =
+  (localStorage.getItem("RightAnswer") * 100) /
+    localStorage.getItem("TotalQuestions") +
+  "%";
 const ProceesBarFunction = () => {
-  ProceesBar.style.width = "10%";
+  ProceesBar.style.width = Parcent.innerText;
 };
 ProceesBarFunction();
 console.log((RightAnswerElement.innerText * 100) / TotalQuestions.innerText);
