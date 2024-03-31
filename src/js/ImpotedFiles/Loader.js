625893755b758c092e609064dda439ec36e5d6e8
Arshnoor Kirmanin <arshnoorkirmani786@gmail.com>
2024-03-31 08:02:05 +0530

    Disabel Right Click and Forword Button block

--- src/js/ImpotedFiles/Loader.js
+++ src/js/ImpotedFiles/Loader.js
@@ -1,3 +1,4 @@
+// =====================================Loader Impelmention=================================
 let LoaderBody = document.createElement("div");
 let Loader = document.createElement("div");
 Loader.classList.add("loader");
@@ -11,3 +12,17 @@ document.querySelector("body").appendChild(LoaderBody);
 window.onload = function () {
   document.querySelector(".LoaderBody").style.display = "none";
 };
+
+// ===================================== Back Forword Logic are Block=================================
+
+window.history.forward(1);
+
+// ===================================== Right Click Logic are Block=================================
+
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+  },
+  false
+);
